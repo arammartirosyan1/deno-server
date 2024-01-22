40809d5050a474ce5856791c67340e86d829bc81
arammartirosyan1 <arammartirosyan788@gmail.com>
2024-01-22 15:57:12 +0400

    htl

--- server.ts
+++ server.ts
@@ -1,1 +1,7 @@
-Deno.serve(() => new Response("Hello Aram"));
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+function handler(_req: Request): Response {
+  return new Response("Hello, World");
+}
+
+serve(handler);
